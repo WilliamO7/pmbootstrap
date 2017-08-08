a92a3362e88bddfbffd1752876ef6ec27d0c1340
Martijn Braam <martijn@brixit.nl>
2017-08-08 22:13:05 +0200

    Replaced apkbuild implementation with shellparser

--- pmb/parse/apkbuild.py
+++ pmb/parse/apkbuild.py
@@ -19,53 +19,7 @@ along with pmbootstrap.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import logging
 import pmb.config
-
-
-def replace_variables(apkbuild):
-    """
-    Replace a hardcoded list of variables inside the APKBUILD.
-    """
-    ret = apkbuild
-    # _flavor: ${_device} (lineageos kernel packages)
-    ret["_flavor"] = ret["_flavor"].replace("${_device}",
-                                            ret["_device"])
-
-    # pkgname: $_flavor
-    ret["pkgname"] = ret["pkgname"].replace("${_flavor}", ret["_flavor"])
-
-    # subpackages: $pkgname
-    replaced = []
-    for subpackage in ret["subpackages"]:
-        replaced.append(subpackage.replace("$pkgname", ret["pkgname"]))
-    ret["subpackages"] = replaced
-
-    # makedepends: $makedepends_host, $makedepends_build, $_llvmver
-    replaced = []
-    for makedepend in ret["makedepends"]:
-        if makedepend.startswith("$"):
-            key = makedepend[1:]
-            if key in ret:
-                replaced += ret[key]
-            else:
-                raise RuntimeError("Could not resolve variable " +
-                                   makedepend + " in APKBUILD of " +
-                                   apkbuild["pkgname"])
-        else:
-            # replace in the middle of the string
-            for var in ["_llvmver"]:
-                makedepend = makedepend.replace("$" + var, ret[var])
-            replaced += [makedepend]
-
-    # Python: ${pkgname#py-}
-    if ret["pkgname"].startswith("py-"):
-        replacement = ret["pkgname"][3:]
-        for var in ["depends", "makedepends", "subpackages"]:
-            for i in range(len(ret[var])):
-                ret[var][i] = ret[var][i].replace(
-                    "${pkgname#py-}", replacement)
-
-    ret["makedepends"] = replaced
-    return ret
+from pmb.parse.shell import ShellParser
 
 
 def cut_off_function_names(apkbuild):
@@ -82,10 +36,8 @@ def cut_off_function_names(apkbuild):
 
 def apkbuild(args, path):
     """
-    Parse relevant information out of the APKBUILD file. This is not meant
-    to be perfect and catch every edge case (for that, a full shell parser
-    would be necessary!). Instead, it should just work with the use-cases
-    covered by pmbootstrap and not take too long.
+    Parse relevant information out of the APKBUILD file.
+    This should process all the relevant edge-cases for APKBUILD parsing.
 
     :param path: Full path to the APKBUILD
     :returns: Relevant variables from the APKBUILD. Arrays get returned as
@@ -96,41 +48,25 @@ def apkbuild(args, path):
     if path in args.cache["apkbuild"]:
         return args.cache["apkbuild"][path]
 
+    # Specify environment variables used by APKBUILD scripts
+    env = {
+        "CARCH": "",
+        "srcdir": "",
+        "CBUILD_ARCH": "",
+        "_kernver": "",
+        "CROSS_COMPILE": ""
+    }
+
     with open(path, encoding="utf-8") as handle:
-        lines = handle.readlines()
+        parsed = ShellParser(handle, environment=env)
 
     # Parse all attributes from the config
     ret = {}
-    for i in range(len(lines)):
-        for attribute, options in pmb.config.apkbuild_attributes.items():
-            if not lines[i].startswith(attribute + "="):
-                continue
-
-            # Extend the line value until we reach the ending quote sign
-            line_value = lines[i][len(attribute + "="):-1]
-            end_char = None
-            if line_value.startswith("\""):
-                end_char = "\""
-            value = ""
-            first_line = i
-            while i < len(lines) - 1:
-                value += line_value.replace("\"", "").strip()
-                if not end_char:
-                    break
-                elif line_value.endswith(end_char):
-                    # This check is needed to allow line break directly after opening quote
-                    if i != first_line or line_value.count(end_char) > 1:
-                        break
-                value += " "
-                i += 1
-                line_value = lines[i][:-1]
-
-            # Split up arrays, delete empty strings inside the list
+    for attribute, options in pmb.config.apkbuild_attributes.items():
+        if attribute in parsed.variables:
+            value = parsed.variables[attribute]
             if options["array"]:
-                if value:
-                    value = list(filter(None, value.split(" ")))
-                else:
-                    value = []
+                value = value.split(" ")
             ret[attribute] = value
 
     # Add missing keys
@@ -154,5 +90,6 @@ def apkbuild(args, path):
                            " the folder, that contains the APKBUILD!")
 
     # Fill cache
+    ret = cut_off_function_names(ret)
     args.cache["apkbuild"][path] = ret
     return ret
--- test/test_bashparser.py
+++ test/test_bashparser.py
@@ -5,7 +5,10 @@ import pytest
 sys.path.append(os.path.abspath(
     os.path.join(os.path.dirname(__file__) + "/..")))
 
+import pmb.helpers.logging
+
 from pmb.parse.shell import ShellParser
+from pmb.parse.apkbuild import apkbuild
 
 
 @pytest.mark.parametrize("shell_script,expected", [
@@ -40,3 +43,45 @@ def test_apkbuild():
 
     shell = ShellParser(open(fixture), environment=env)
     assert shell.variables["_flavor"] == "postmarketos"
+
+
+@pytest.fixture
+def args(request):
+    import pmb.parse
+    sys.argv = ["pmbootstrap.py", "chroot"]
+    args = pmb.parse.arguments()
+    args.log = args.work + "/log_testsuite.txt"
+    pmb.helpers.logging.init(args)
+    request.addfinalizer(args.logfd.close)
+    return args
+
+
+def test_apkbuild_parser(args):
+    fixture = "../aports/main/linux-postmarketos/APKBUILD"
+
+    result = apkbuild(args, fixture)
+    assert result["pkgname"] == "linux-postmarketos"
+
+    assert isinstance(result["arch"], list)
+    assert len(result["arch"]) == 1
+    assert result["arch"][0] == "all"
+
+    assert isinstance(result["depends"], list)
+    assert len(result["depends"]) == 1
+    assert result["depends"][0] == "postmarketos-mkinitfs"
+
+    assert isinstance(result["makedepends"], list)
+    assert len(result["makedepends"]) == 8
+    assert set(result["makedepends"]) == {"perl", "sed", "installkernel", "bash", "gmp-dev", "bc", "linux-headers",
+                                          "elfutils-dev"}
+
+    assert isinstance(result["options"], list)
+    assert len(result["options"]) == 3
+    assert set(result["options"]) == {"!strip", "!check", "!tracedeps"}
+
+    assert result["pkgrel"] == "4"
+    assert result["pkgver"] == "4.12.4"
+
+    assert isinstance(result["subpackages"], list)
+    assert len(result["subpackages"]) == 1
+    assert set(result["subpackages"]) == {"linux-postmarketos-dev"}
