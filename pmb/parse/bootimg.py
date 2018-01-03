b440532af23fdaec5d3e97541bc4abedbb86315c
William O-D <obirikscp@yahoo.co.uk>
2018-01-03 23:28:47 +0000

    sony-jlo: changes # 6
    i need sleep

--- pmb/parse/bootimg.py
+++ pmb/parse/bootimg.py
@@ -54,7 +54,7 @@ def bootimg(args, path):
                                " flash method. See also: "
                                "<https://wiki.postmarketos.org/wiki/Deviceinfo_flash_methods>")
         elif check_sonyelf(bootimg_path, file_output.lower()) is True:
-           bootimg_is_sonyelf = True
+            bootimg_is_sonyelf = True
             # We have a Sony Xperia ELF format boot image, which some devices
             # (such as the Xperia J) have. We need special tools to deal with these,
             # namely unpackelf to get the required offsets.
